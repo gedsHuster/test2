8403f494b05a90a1c3e94f16257763281d7a7c4e
gedsHuster <35480033+gedsHuster@users.noreply.github.com>
2018-11-16 23:05:44 +0800

    Update struct2excel.m

--- struct2excel.m
+++ struct2excel.m
@@ -1,16 +1,16 @@
-%% 将结构体保存到excel
+%% 灏嗙粨鏋勪綋淇濆瓨鍒癳xcel
 clc;clear; 
 s.China = [1 2 3]'; 
 s.US = [1 2 3 4]'; 
 s.UK = [1 2 3 4 5]';
-cell = struct2cell(s);% struct转成cell
+cell = struct2cell(s);% struct杞垚cell
 for sheet=1:3
     xlswrite('output_struct.xlsx', cell{sheet}, sheet);
 end
 sheet_name = fieldnames(s);
-%% 改sheet名字
+%% 鏀箂heet鍚嶅瓧
 e = actxserver('Excel.Application'); % # open Activex server
-ewb = e.Workbooks.Open('C:\Users\geds\Desktop\output_struct.xlsx'); % # open file (enter full path!)
+ewb = e.Workbooks.Open('your-own-path\output_struct.xlsx'); % # open file (enter full path!)
 for sheet = 1:3
     ewb.Worksheets.Item(sheet).Name = sheet_name{sheet}; % # rename 1st sheet
 end
